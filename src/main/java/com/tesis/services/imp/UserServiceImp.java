0b1fc66b55514fccc9345af2fa69f0f7246a66d2
Nicolas <ncargnelutti104@alumnos.iua.edu.ar>
2019-04-02 19:33:12 -0300

    GET users/:user_id

--- src/main/java/com/tesis/services/imp/UserServiceImp.java
+++ src/main/java/com/tesis/services/imp/UserServiceImp.java
@@ -4,6 +4,7 @@ import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import com.tesis.jooq.tables.daos.UsersDao;
 import com.tesis.jooq.tables.pojos.Devices;
+import com.tesis.jooq.tables.pojos.Users;
 import com.tesis.models.ResponseDTO;
 import com.tesis.services.UserService;
 
@@ -15,7 +16,13 @@ public class UserServiceImp implements UserService {
     @Inject
     UsersDao usersDao;
 
-    public ResponseDTO<List<Devices>> getUsers() {
+    public ResponseDTO<List<Users>> getUsers() {
         return new ResponseDTO(usersDao.findAll(), null);
+    }
+
+    public ResponseDTO<List<Users>> getUsersByUserID(Long userID) {
+        ResponseDTO<List<Users>> responseDTO = new ResponseDTO();
+        responseDTO.model = usersDao.fetchById(userID);
+        return responseDTO;
     }
 }
