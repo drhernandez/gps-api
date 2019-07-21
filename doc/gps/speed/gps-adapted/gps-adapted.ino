aef7a392f2b574bf61e0b3e968fd1be481326ff7
Nicolas <ncargnelutti104@alumnos.iua.edu.ar>
2019-07-21 11:13:51 -0300

    first speed commit

--- doc/gps/speed/gps-adapted/gps-adapted.ino
+++ doc/gps/speed/gps-adapted/gps-adapted.ino
@@ -21,38 +21,21 @@ const int MEMORY_SIZE = (strlen(EXAMPLE)) * SIZE * sizeof(char) + 1;
 time_t prevDisplay = 0; // when the digital clock was displayed
 
 void setup() {
-  Serial.begin(9600); //115200
+  Serial.begin(9600);
   gsm_gprs.begin(19200);   // Setting the baud rate of GSM Module
   delay(1000);
-  Serial.write("iniciando");
+  Serial.write("iniciando\n");
   init_gprs_module();
   delay(500);
-  //Serial.begin(19200); //115200
   softSerial.begin(9600); //9600
 }
 
 void loop() {
   bool newData = false;
-  unsigned long chars;
-  unsigned short sentences, failed;
-  float flat, flon;
-  flat = -31.4109;
-  flon = -64.1897;
   buildWeft();
   Serial.write("finishhhh\n");
   delay(500);
-/*  
-    gps.stats(&chars, &sentences, &failed);
-    Serial.print(" CHARS=");
-    Serial.print(chars);
-    Serial.print(" SENTENCES=");
-    Serial.print(sentences);
-    Serial.print(" CSUM ERR=");
-    Serial.println(failed);
 
-    if (chars == 0)
-      Serial.println("** No characters received from GPS: check wiring **");
-*/  
 }
 
 void init_gprs_module() {
@@ -77,6 +60,7 @@ void init_gprs_module() {
 }
 
 void send_http_post(char *coordinate_data) {
+  /*
   gsm_gprs.println("AT+HTTPINIT");
   delay(100);
   printSerialData();
@@ -102,6 +86,7 @@ void send_http_post(char *coordinate_data) {
   delay(100);
   printSerialData();
   delay(5000);
+  */
   Serial.println(coordinate_data);
 }
 
@@ -143,7 +128,7 @@ void buildWeft() {
     Serial.println("La aplicacion no pudo reservar memoria y se va a cerrar!");
     exit(EXIT_FAILURE);
   }
-  // data mocked
+//  ** data mocked **
 //  for (unsigned int start = 0; start <= 9; start ++) {
 //    parseCoordinates(-31.4109, -64.1897, 8, 246, DEVICE_ID, coordinate_data);
 //  }
@@ -153,16 +138,16 @@ void buildWeft() {
       for (unsigned long start = millis(); millis() - start < 1000;) {
         while (softSerial.available()) {
           char c = softSerial.read();
-          //Serial.write(c); // uncomment this line to see the GPS data
+          Serial.write(c); // uncomment this line to see the GPS data
           if (gps.encode(c)) // validating the sentence
             newData = true;
         }
       }
       if (newData) {
-      float flat, flon;
-      unsigned long age;
-      int Year;
-      byte Month, Day, Hour, Minute, Second;
+      //float flat, flon;
+      //unsigned long age;
+      //int Year;
+      //byte Month, Day, Hour, Minute, Second;
       gps.crack_datetime(&Year, &Month, &Day, &Hour, &Minute, &Second, NULL, &age);
       gps.f_get_position(&flat, &flon, &age);
       if (age < 500) {
@@ -176,14 +161,12 @@ void buildWeft() {
           prevDisplay = now();
         }
       }
-      //logData(flat, flon);
+      logData(flat, flon);
       parseCoordinates(flat, flon, gps.satellites(), gps.hdop(), DEVICE_ID, coordinate_data);
      }
     }
 
-  //sendData(coordinate_data);
   send_http_post(coordinate_data);
-  //delay(500);
   free(coordinate_data);
 }
 
@@ -192,6 +175,10 @@ void logData(float flat, float flon) {
   Serial.print(flat == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : flat, 6);
   Serial.print(" LON=");
   Serial.print(flon == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : flon, 6);
+  Serial.print(" SPEED=");
+  Serial.print(gps.f_speed_kmph() == TinyGPS::GPS_INVALID_F_SPEED ? 0.0 : gps.f_speed_kmph());
+  Serial.print(" LONG_SPEED=");
+  Serial.print(gps.speed() == TinyGPS::GPS_INVALID_F_SPEED ? 0.0 : gps.speed()*0.0185);
   Serial.print(" SAT=");
   Serial.print(gps.satellites() == TinyGPS::GPS_INVALID_SATELLITES ? 0 : gps.satellites());
   Serial.print(" HDOP=");
@@ -201,14 +188,7 @@ void logData(float flat, float flon) {
   char sz[30];
   sprintf(sz, "%02d-%02d-%02dT%02d:%02d:%02d:000-03:00",
           day(), month(), year(), hour(), minute(), second());
-  Serial.print(sz);
-}
-
-void sendData(char *coordinate_data) {
-  // post to the service
-  // ...
-  send_http_post(coordinate_data);
-  //Serial.println(coordinate_data);
+  Serial.println(sz);
 }
 
 void printSerialData() {
