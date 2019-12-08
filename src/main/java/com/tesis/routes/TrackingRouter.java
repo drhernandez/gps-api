6b859f93da0fefb68592c92870899e06fa421a7d
Nicolas <ncargnelutti104@alumnos.iua.edu.ar>
2019-12-08 20:35:03 -0300

    remove commented lines

--- src/main/java/com/tesis/routes/TrackingRouter.java
+++ src/main/java/com/tesis/routes/TrackingRouter.java
@@ -24,22 +24,10 @@ public class TrackingRouter implements RouteGroup {
     Logger logger = LoggerFactory.getLogger(TrackingRouter.class);
 
     private TrackingController trackingController;
-    private AlertController alertController;
-    private AlertService alertService;
-    private AuthService authService;
-    private Middlewares middlewares;
 
     @Inject
-    public TrackingRouter(TrackingController trackingController,
-                          AlertController alertController,
-                          AlertService alertService,
-                          AuthService authService,
-                          Middlewares middlewares) {
+    public TrackingRouter(TrackingController trackingController) {
         this.trackingController = trackingController;
-        this.alertController = alertController;
-        this.alertService = alertService;
-        this.authService = authService;
-        this.middlewares = middlewares;
     }
 
     @Override
@@ -53,78 +41,5 @@ public class TrackingRouter implements RouteGroup {
             Spark.get("/search", trackingController::trackingSearch);
             Spark.get("/:device_id", trackingController::getTrackingsByDeviceID);
         });
-
-//        Spark.after("/trackings",((request, response) -> {
-//            if (request.requestMethod().equals("POST") && response.status() == 200) {
-//                List<Trackings> trackingsList = JsonUtils.INSTANCE.GSON().fromJson(response.body(), new TypeToken<List<Trackings>>(){}.getType());
-//                SpeedAlerts speedAlert = alertService.getSpeedIfActive(trackingsList.get(0).getDeviceId());
-//                if(speedAlert != null) {
-//                    for(int i=0; i<trackingsList.size(); i++){
-//                        if(trackingsList.get(i).getSpeed() > speedAlert.getSpeed() &&
-//                                trackingsList.get(i).getTime().after(speedAlert.getActivatedAt())){
-//                            alertService.createSpeedAlertHistory(new SpeedAlertsHistory(
-//                                    trackingsList.get(i).getTime(),
-//                                    speedAlert.getId(),
-//                                    trackingsList.get(i).getSpeed()));
-//                            try {
-//                                alertController.sendAlarm(trackingsList.get(i).getDeviceId(),"SPEED");
-//                            } catch (Exception e) {
-//                                halt(500, e.getMessage());
-//                            }
-//                            break;
-//                        }
-//                    }
-//                }
-//
-//                MovementAlerts movementAlert = alertService.getMovementIfActive(trackingsList.get(0).getDeviceId());
-//                if(movementAlert != null) {
-//
-//                    for(int i=0; i<trackingsList.size(); i++) {
-//                        if(trackingsList.get(i).getTime().after(movementAlert.getActivatedAt()) &&
-//                                checkDistance(trackingsList.get(i), movementAlert)){
-//                            alertService.createMovementAlertHistory(new MovementAlertsHistory(
-//                                    trackingsList.get(i).getTime(),
-//                                    movementAlert.getId(),
-//                                    trackingsList.get(i).getLat(),
-//                                    trackingsList.get(i).getLng()
-//                            ));
-//                            try {
-//                                alertController.sendAlarm(trackingsList.get(i).getDeviceId(),"MOVEMENT");
-//                            } catch (Exception e) {
-//                                halt(500, e.getMessage());
-//                            }
-//                            break;
-//                        }
-//                    }
-//
-//                }
-//            }
-//        }));
     }
-
-//    public boolean checkDistance(Trackings tracking, MovementAlerts alert){
-//
-//        double lat1 = Math.toRadians(tracking.getLat());
-//        double lng1 = Math.toRadians(tracking.getLng());
-//        double lat2 = Math.toRadians(alert.getLat());
-//        double lng2 = Math.toRadians(alert.getLng());
-//
-//        // Haversine equation
-//        double a = Math.pow(Math.sin((lat2 - lat1) / 2), 2) + Math.cos(lat1) * Math.cos(lat2) * Math.pow(Math.sin((lng2 - lng1) / 2),2);
-//
-//        return (2 * Math.asin(Math.sqrt(a)) * EARTH_RADIUS_KM) > DEFAULT_MOVEMENT_ALERT_KM;
-//
-//    }
 }
-
-
-//normal alert
-//10001,-31.4109,-64.1897,1.0,8,246,25-09-2019T20:51:09:000-03:00;10001,-31.4109,-64.1897,1.0,8,246,25-09-2019T20:51:09:000-03:00;10001,-31.4109,-64.1897,1.0,8,246,25-09-2019T20:51:09:000-03:00;10001,-31.4109,-64.1897,1.0,8,246,25-09-2019T20:51:09:000-03:00;10001,-31.4109,-64.1897,1.0,8,246,25-09-2019T20:51:09:000-03:00
-
-
-//movement alert
-//10001,-31.4109,-64.1897,10.0,8,246,25-09-2019T20:51:09:000-03:00;10001,-31.4109,-64.1897,10.0,8,246,25-09-2019T20:51:09:000-03:00;10001,-31.4109,-64.1897,10.0,8,246,25-09-2019T20:51:09:000-03:00;10001,-31.4109,-64.1897,10.0,8,246,25-09-2019T20:51:09:000-03:00;10001,-31.4109,-64.1897,10.0,8,246,25-09-2019T20:51:09:000-03:00
-
-
-//speed alert
-//10001,-31.4109,-64.1897,100.0,8,246,25-09-2019T20:51:09:000-03:00;10001,-31.4109,-64.1897,100.0,8,246,25-09-2019T20:51:09:000-03:00;10001,-31.4109,-64.1897,100.0,8,246,25-09-2019T20:51:09:000-03:00;10001,-31.4109,-64.1897,100.0,8,246,25-09-2019T20:51:09:000-03:00;10001,-31.4109,-64.1897,100.0,8,246,25-09-2019T20:51:09:000-03:00
