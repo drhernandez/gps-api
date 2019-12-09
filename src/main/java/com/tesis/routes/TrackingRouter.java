c6bfa7b5f91829426f4ad6e2e0274d37d4f1b21f
Nicolas <ncargnelutti104@alumnos.iua.edu.ar>
2019-12-09 18:05:21 -0300

    remove unused imports

--- src/main/java/com/tesis/routes/TrackingRouter.java
+++ src/main/java/com/tesis/routes/TrackingRouter.java
@@ -1,23 +1,11 @@
 package com.tesis.routes;
 
-import com.google.gson.reflect.TypeToken;
 import com.google.inject.Inject;
-import com.tesis.controllers.AlertController;
 import com.tesis.controllers.TrackingController;
-import com.tesis.jooq.tables.pojos.*;
-import com.tesis.services.AlertService;
-import com.tesis.services.AuthService;
-import com.tesis.utils.JsonUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import spark.RouteGroup;
 import spark.Spark;
-
-import java.util.List;
-
-import static com.tesis.config.Constants.DEFAULT_MOVEMENT_ALERT_KM;
-import static com.tesis.config.Constants.EARTH_RADIUS_KM;
-import static spark.Spark.halt;
 
 public class TrackingRouter implements RouteGroup {
 
